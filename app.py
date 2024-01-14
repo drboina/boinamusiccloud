a09a885d05b67fe1d3b295a2d71eb448ed93258f
Leonardo Baez Castillo <boinas@192.168.0.109>
2024-01-14 00:05:48 -0700

    v1.0

---
+++ app.py
@@ -0,0 +1,39 @@
+import io
+import os
+import wave
+import numpy as np
+from scipy.io import wavfile
+import streamlit as st
+
+AUDIO_EXTENSIONS = ["wav", "flac", "mp3", "aac", "ogg", "oga", "m4a", "opus", "wma"]
+
+st.image("logo_chico.png")
+
+st.header("Mix Recordings")
+
+def get_audio_files_in_dir(directory):
+    out = []
+    for item in os.listdir(directory):
+        try:
+            name, ext = item.split(".")
+        except:
+            continue
+        if name and ext:
+            if ext in AUDIO_EXTENSIONS:
+                out.append(item)
+    return out
+
+avdir = os.path.expanduser("/Users/boinas/Documents/soundcloudboina/boinas/")
+audiofiles = get_audio_files_in_dir(avdir)
+
+if len(audiofiles) == 0:
+    st.write("No hay archivos")
+
+else:
+    filename = st.selectbox(
+        "Play it!",
+        audiofiles,
+        0,
+    )
+    audiopath = os.path.join(avdir, filename)
+    st.audio(audiopath)
